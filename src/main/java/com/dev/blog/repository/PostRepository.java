860f1849880cb89ec104ee26803a67239f07c9b5
IagoLSJ <joseiagolima@alu.ufc.br>
2024-06-07 13:05:36 -0300

    feat: add new fuctions in post service & tests of postService

--- src/main/java/com/dev/blog/repository/PostRepository.java
+++ src/main/java/com/dev/blog/repository/PostRepository.java
@@ -2,8 +2,12 @@ package com.dev.blog.repository;
 
 import com.dev.blog.model.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<PostEntity, UUID> {
+    @Query("SELECT p FROM post p WHERE p.title LIKE %:title%")
+    List<PostEntity> findByTitle(String title);
 }
