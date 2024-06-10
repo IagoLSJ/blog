308106dc81656a7c0b33f7386c4d694dbd5627d2
IagoLSJ <joseiagolima@alu.ufc.br>
2024-06-10 12:34:35 -0300

    feat: add new functions in service and add new class of test service

--- src/main/java/com/dev/blog/service/TagService.java
+++ src/main/java/com/dev/blog/service/TagService.java
@@ -2,6 +2,8 @@ package com.dev.blog.service;
 
 import com.dev.blog.DTO.Tag.AddTagAtPostDTO;
 import com.dev.blog.DTO.Tag.CreateTagDTO;
+import com.dev.blog.DTO.Tag.UpdateTagDTO;
+import com.dev.blog.configs.ResourceNotFoundException;
 import com.dev.blog.model.TagEntity;
 import com.dev.blog.repository.PostRepository;
 import com.dev.blog.repository.TagRepository;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.UUID;
 
 @Service
 public class TagService {
@@ -52,4 +55,40 @@ public class TagService {
     public List<TagEntity> findAll(){
         return this.tagRepository.findAll();
     }
+
+    public TagEntity findById(UUID id){
+        var tagById = this.tagRepository.findById(id);
+
+        if(!tagById.isPresent()){
+            throw new ResourceNotFoundException("Tag not found");
+        }
+
+        return tagById.get();
+    }
+
+    public List<TagEntity> findByName(String title){
+        return this.tagRepository.findByName(title);
+    }
+
+
+    public TagEntity update(UpdateTagDTO updateTagDTO, UUID id){
+        TagEntity tagById = this.findById(id);
+
+        if(tagById.getName() == updateTagDTO.getName()){
+            throw new ResourceNotFoundException("Title already in use");
+        }
+
+        tagById.setName(updateTagDTO.getName());
+
+        this.tagRepository.save(tagById);
+        return tagById;
+    }
+
+    public void delete(UUID id){
+        var tagById = this.findById(id);
+
+        this.tagRepository.delete(tagById);
+    }
+
+
 }
